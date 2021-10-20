0ce9f87a7e4e7cd91329420b06fd52c1c58a05b3
ahiya <ahiya.kalfon@gmail.com>
2021-10-20 14:06:39 +0300

    starting login

--- YadBeYadAPP/Services/YadBeYadAPIProxy.cs
+++ YadBeYadAPP/Services/YadBeYadAPIProxy.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -99,6 +100,33 @@ namespace YadBeYadApp.Services
                     string content = await response.Content.ReadAsStringAsync();
                     
                     return content;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+        public async Task<User> LoginAsync(string email, string pass)
+        {
+            try
+            {
+                HttpResponseMessage response = await this.client.GetAsync($"{this.baseUri}/Login?email={email}&pass={pass}");
+                if (response.IsSuccessStatusCode)
+                {
+                    JsonSerializerOptions options = new JsonSerializerOptions
+                    {
+                        ReferenceHandler = ReferenceHandler.Preserve, //avoid reference loops!
+                        PropertyNameCaseInsensitive = true
+                    };
+                    string content = await response.Content.ReadAsStringAsync();
+                    User u = JsonSerializer.Deserialize<User>(content, options);
+                    return u;
                 }
                 else
                 {
--- YadBeYadAPP/ViewModels/LoginPageViewModel.cs
+++ YadBeYadAPP/ViewModels/LoginPageViewModel.cs
@@ -4,7 +4,8 @@ using System.Text;
 
 namespace YadBeYadAPP.ViewModels
 {
-    class Class1
+    class LoginPageViewModel
     {
+        //hgjm
     }
 }
